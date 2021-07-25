c8cc0d5ca955d286932cffee10ee9ac9b2256b1f
SANDEEP DEY <72209045+122Sandeep@users.noreply.github.com>
2021-07-25 12:22:02 +0530

    Add files via upload

---
+++ php/dbcon.php
@@ -0,0 +1,15 @@
+<?php  
+$servername= "localhost";
+$username= "id17303713_anshul66";
+$password="IR5zuu%EC@bK{|ev";
+$dbName="id17303713_pwchat";
+$con= new mysqli($servername,$username,$password,$dbName);
+if ($con->connect_error) 
+{
+	echo "<h2>Sorry We Have Some Problem In The Server</h2>";
+}
+?>
+
+
+
+
